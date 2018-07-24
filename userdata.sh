cb80032af14da861a28456bfefd3f63f9d2e7c8e
Tsukada <seiya.tsukada@accenture.com>
2018-07-24 16:43:24 +0900

    commit

--- userdata.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd /usr/local/src
-aws s3 cp s3://my-provision.whiteray.tokyo/userdata.sh ./
-
-# https://qiita.com/szk3/items/a3e054527c41d071ec94
-# ansible 
-# ansible-playbook site.yml -i [server_ip]
