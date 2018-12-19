4c4e1577d31430db9022a0c76e60110e76d9cef0
Tsukada <seiya.tsukada@accenture.com>
2018-12-19 17:15:23 +0900

    commit

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+BUCKET_NAME="my-provision.whiteray.tokyo"
+
 /usr/bin/yum -y install git
 
 /usr/bin/pip install --upgrade pip
 /usr/local/bin/pip install ansible
 
-/usr/bin/aws s3 cp s3://my-provision.whiteray.tokyo/git-keys/id_git_rsa ~/.ssh/id_rsa
-/usr/bin/aws s3 cp s3://my-provision.whiteray.tokyo/ssh/config ~/.ssh/config
-/usr/bin/aws s3 cp s3://my-provision.whiteray.tokyo/provision/provision.yml /tmp/provision.yml
+/usr/bin/aws s3 cp s3://${BUCKET_NAME}/git-keys/id_git_rsa ~/.ssh/id_rsa
+/usr/bin/aws s3 cp s3://${BUCKET_NAME}/ssh/config ~/.ssh/config
+/usr/bin/aws s3 cp s3://${BUCKET_NAME}/provision/provision.yml /tmp/provision.yml
 
 /bin/echo -e "[server]\n127.0.0.1" > /tmp/hosts
 /usr/local/bin/ansible-playbook -i /tmp/hosts /tmp/provision.yml --connection=local
@@ -15,8 +17,4 @@
 /bin/chmod 400 ~/.ssh/id_rsa
 /bin/chmod 400 ~/.ssh/config
 
-/usr/bin/git clone git@github.com:seiya-tsukada/my_aws_provision.git ~/my_aws_provision
-
-
-
-
+# /usr/bin/git clone git@github.com:seiya-tsukada/my_aws_provision.git ~/my_aws_provision
